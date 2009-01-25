3a0520a81a14fd1a0bdbed06678aab88440e39e3
Tim.Lebedkov <Tim.Lebedkov@d00f100a-dc9f-11dd-9059-71fac19e68c6>
2009-01-25 20:05:56 +0000

    Bugfixes & tests for .toMap/.fromMap

--- trunk/JAU/test/com/googlecode/jau/equals/package-info.java
+++ trunk/JAU/test/com/googlecode/jau/equals/package-info.java
@@ -3,11 +3,13 @@
 @JAUHashCode
 @JAUToString
 @JAUCopy
+@JAUToMap
 package com.googlecode.jau.equals;
 
 import com.googlecode.jau.JAUCompareTo;
 import com.googlecode.jau.JAUCopy;
 import com.googlecode.jau.JAUEquals;
 import com.googlecode.jau.JAUHashCode;
+import com.googlecode.jau.JAUToMap;
 import com.googlecode.jau.JAUToString;
 
