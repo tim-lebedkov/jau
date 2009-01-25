fb92cee0c5dfec44b1338793b6d739a5a360efcf
Tim Lebedkov <tim-lebedkov@users.noreply.github.com>
2009-01-25 20:05:40 +0000

    Bugfixes & tests for .toMap/.fromMap

---
+++ JAU/test/com/googlecode/jau/ClassTwo.java
@@ -0,0 +1,28 @@
+package com.googlecode.jau;
+
+@JAUToMap
+public class ClassTwo {
+    public byte byte_;
+    public short short_;
+    public int int_;
+    public long long_;
+    public float float_;
+    public double double_;
+    public String string_;
+
+    public byte[] bytea;
+    public short[] shorta;
+    public int[] inta;
+    public long[] longa;
+    public float[] floata;
+    public double[] doublea;
+    public String[] stringa;
+
+    public byte[][] bytea2;
+    public short[][] shorta2;
+    public int[][] inta2;
+    public long[][] longa2;
+    public float[][] floata2;
+    public double[][] doublea2;
+    public String[][] stringa2;
+}
