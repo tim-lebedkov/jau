90a175600bdf56b23ac273276eb0d01200b4a8ff
Tim.Lebedkov <Tim.Lebedkov@d00f100a-dc9f-11dd-9059-71fac19e68c6>
2009-01-29 23:27:15 +0000

    infrastructure for performance tests

---
+++ trunk/JAU/test/com/googlecode/jau/ToStringPerfTest.java
@@ -0,0 +1,22 @@
+package com.googlecode.jau;
+
+import com.sun.japex.*;
+
+/**
+ * Performance tests.
+ */
+public class ToStringPerfTest extends JapexDriverBase {
+    private int[] object;
+
+    @Override
+    public void prepare(TestCase testCase) {
+        object = new int[1024];
+        for (int i = 0; i < object.length; i++) {
+            object[i] = i;
+        }
+    }
+
+    public void run(TestCase testCase) {
+        String s = JAU.toString(object);
+    }
+}
